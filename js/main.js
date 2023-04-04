bfd499462f71112af435131f5fac35ec768173a9
Selanis <k.polexowa@yandex.ru>
2023-04-04 17:42:24 +0300

    js изменила

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 var buttonSlider = document.querySelector(".button-right");
-
-var mainPictContent = document.querySelectorAll("header .main-pict-content div")
+var mainPictContent = document.querySelectorAll("header .main-pict-content .div-images img");
+var Slider = document.querySelectorAll(".scroler div");
 var counter = 0;
+console.log(Slider);
 
 buttonSlider.onclick = function() {
-    mainPictContent[counter].className = "hiden";
+    mainPictContent[counter].className = "hidden";
+    Slider[counter].className = "item2";
     counter++;
     if (counter >= mainPictContent.length) {
         counter = 0;
     };
     
     mainPictContent[counter].className = "shown";
+    Slider[counter].className = "item1";
 };
 
-console.log("Работает");
