3ef17bbd7d46e604899fc605195bfd5ce21ac97d
Selanis <k.polexowa@yandex.ru>
2023-04-07 15:10:33 +0300

    Доделала сортировку, начала делать другую секцию

--- js/main.js
+++ js/main.js
@@ -19,9 +19,9 @@ buttonSlider.onclick = function() {
 
 // Выпадающий список
 var buttonShow = document.querySelector('.see-all');
-var secondPage = document.querySelectorAll(".second-sort-page .grid-sec");
-
-console.log(secondPage);
+var secondPage = document.querySelectorAll(".second-sort-page2 .grid-sec");
+var thirdPage = document.querySelectorAll(".third-sort-page .grid-sec");
+var firstPage = document.querySelectorAll(".first-sort-page .grid-sec");
 
 buttonShow.onclick = function() {
     if (buttonShow.classList.length == 1) {
@@ -29,16 +29,69 @@ buttonShow.onclick = function() {
         buttonShow.innerHTML = "Hide All Collections";
         for (i = 3; i < 9; i++) {
             secondPage[i].classList.remove("grid-hidden");
+            thirdPage[i].classList.remove("grid-hidden");
+            firstPage[i].classList.remove("grid-hidden");
         }
     } else {
         buttonShow.classList.remove("see-all-shown");
         buttonShow.innerHTML = "See All Collections"
         for (i = 3; i < 9; i++) {
             secondPage[i].classList.add("grid-hidden");
+            thirdPage[i].classList.add("grid-hidden");
+            firstPage[i].classList.add("grid-hidden");
         }
     };
 }
 
+// Сортировка
+button1DAY = document.getElementById("day1");
+button7DAYS = document.getElementById("days7");
+button30DAYS = document.getElementById("days30");
+sortList = document.querySelector(".list-pages");
+console.log(sortList);
+
+button1DAY.onclick = function() {
+    if (sortList.classList.contains("visible1") === false) {
+        sortList.classList.remove(sortList.classList[1]);
+        sortList.classList.add("visible1");
+
+        if (button7DAYS.classList.length == 2) {
+            button7DAYS.classList.remove("shown-list");
+        } else if (button30DAYS.classList.length == 2) {
+            button30DAYS.classList.remove("shown-list")
+        };
+        button1DAY.classList.add("shown-list");
+    } else { };
+}
+
+button7DAYS.onclick = function() {
+    if (sortList.classList.contains("visible7") === false) {
+        sortList.classList.remove(sortList.classList[1]);
+        sortList.classList.add("visible7");
+
+        if (button1DAY.classList.length == 2) {
+            button1DAY.classList.remove("shown-list");
+        } else if (button30DAYS.classList.length == 2) {
+            button30DAYS.classList.remove("shown-list")
+        };
+        button7DAYS.classList.add("shown-list");
+    } else { };
+}
+
+button30DAYS.onclick = function() {
+    if (sortList.classList.contains("visible30") === false) {
+        sortList.classList.remove(sortList.classList[1]);
+        sortList.classList.add("visible30");
+
+        if (button1DAY.classList.length == 2) {
+            button1DAY.classList.remove("shown-list");
+        } else if (button7DAYS.classList.length == 2) {
+            button7DAYS.classList.remove("shown-list")
+        };
+        button30DAYS.classList.add("shown-list");
+    } else { };
+}
+
 // Окно всплывающее
-var Searchbutton = document.querySelector(".search .submit");
-Searchbutton.onclick = alert("Принято");
+// var Searchbutton = document.querySelector(".search .submit");
+// Searchbutton.onclick = alert("Принято");
